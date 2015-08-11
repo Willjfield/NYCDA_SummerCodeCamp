444042daff1f1b143523534795acdfd285ec563f
Will Field <willjfield@gmail.com>
2015-08-10 23:35:05 -0400

    jquery

---
+++ class_07/jqueryExample/scripts/main.js
@@ -0,0 +1,13 @@
+//When the document is ready
+$('document').ready(function(){
+	//Find the element that has the id "mydiv". Then take click events on that element.	
+	$('#mydiv').click(
+		//When it has been clicked, execute the following (nameless aka "anonymous") function
+		function(){
+			//The anonymous function will look for the element with the id "mydiv" and preform the hide() function on it that we happen to have looked up in the jquery documentation.
+			$('#mydiv').hide();
+			//Also, just to check, console log the word "hidden"
+			console.log("hidden");
+		}
+	)
+});
